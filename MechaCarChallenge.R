23b1c68ffada9849495dce92f537e7d80ec83800
Panagioti (Pete) Tsivis <87956455+Greekman12490@users.noreply.github.com>
2021-11-21 18:10:49 -0500

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -16,3 +16,16 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar))
 
+#Dev 2
+
+#Reading the "Suspension_Coil.csv" file
+
+MechaSuspension <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+#Creating a dataframe named "total_summary" that gets the mean,median,variance and SD of PSI column
+
+total_summary <- MechaSuspension %>% summarize(Mean = mean(PSI),Median = median(PSI),Variance = var(PSI),SD = sd(PSI))
+
+#Creating a dataframe name "lot_summary" that gets the same stats as the DF total_summary, but groups it by lot
+
+lot_summary <- MechaSuspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI),Median = median(PSI),Variance = var(PSI),SD = sd(PSI), .groups = 'keep') 
